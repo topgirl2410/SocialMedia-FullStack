7b136c532af16a5f877fce7b200d7de2c07ab98e
topgirl2410 <maria.topgirl82@gmail.com>
2023-06-05 23:04:05 +0200

    login y hasheo de pass

--- api/index.js
+++ api/index.js
@@ -4,12 +4,16 @@ import postRoutes from "./routes/posts.js";
 import likeRoutes from "./routes/likes.js";
 import commentRoutes from "./routes/comments.js";
 import authRoutes from "./routes/auth.js";
+import cookieParser from "cookie-parser";
+import cors from 'cors';
 
 const app = express();
 
 
 //TODO: middlewares
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
 
 app.use("/api/users", userRoutes);
