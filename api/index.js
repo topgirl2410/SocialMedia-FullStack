c937a877f36252e51319cf8dd8995f0ca582e167
topgirl2410 <maria.topgirl82@gmail.com>
2023-06-05 22:26:54 +0200

    registro creado

--- api/index.js
+++ api/index.js
@@ -1,6 +1,23 @@
 import express from "express";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import likeRoutes from "./routes/likes.js";
+import commentRoutes from "./routes/comments.js";
+import authRoutes from "./routes/auth.js";
 
-const app = express()
+const app = express();
+
+
+//TODO: middlewares
+app.use(express.json())
+
+
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/likes", likeRoutes);
+app.use("/api/auth", authRoutes);
+
 
 
 app.listen(4000, () => {
